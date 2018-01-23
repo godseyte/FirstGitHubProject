290eb0a0e1ed24fa431dc0d7456205b3f0afdd07
godseyte <godseyte@IL-PC-149.uccc.uc.edu>
2018-01-23 11:50:54 -0500

    My First GitHub commit I've ever done

---
+++ FirstGitHubProject/src/gitHub/Main.java
@@ -0,0 +1,15 @@
+package gitHub;
+
+/**
+ * First project uploaded to GitHub to learn how to upload projects to GitHub
+ * @author godseyte
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Here we are.");
+	}
+
+}
